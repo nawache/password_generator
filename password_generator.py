5b6bbf23834af46f0d14dfacb657e15256be7a2b
Pavel Chembulatov <mr.chembulatov@yandex.ru>
2024-10-27 23:04:49 +0500

    Improve password security

--- password_generator.py
+++ password_generator.py
@@ -2,9 +2,10 @@ import secrets
 import string
 
 
-valid_symbols = string.ascii_letters + string.digits + '!@#$%^&*()'
+VALID_SYMBOLS = string.ascii_letters + string.digits + '!@#$%^&*()'
 ERROR_MESSAGE = ("Значение длины пароля должно быть целым положительным "
                  "числом от 1 до 64.")
+POSITIVE_ANSWERS = ("д", "да", "ага", "y", "yes", "yeah")
 
 
 def generate_password(length, symbols):
@@ -15,9 +16,9 @@ def generate_password(length, symbols):
 def get_password_length():
     while True:
         try:
-            length = int(input("\nВведите длину пароля:\n>: "))
-            if length > 0 and length <= 64:
-                return length
+            password_length = int(input("\nВведите длину пароля:\n>: "))
+            if password_length > 0 and password_length <= 64:
+                return password_length
             else:
                 print(ERROR_MESSAGE)
         except ValueError:
@@ -26,15 +27,13 @@ def get_password_length():
 
 def main():
     length = get_password_length()
-    password = generate_password(length, valid_symbols)
+    password = generate_password(length, VALID_SYMBOLS)
 
     print("\nСгенерированный пароль:", password)
     try_again()
 
 
 def try_again():
-    POSITIVE_ANSWERS = ("д", "да", "ага", "y", "yes", "yeah")
-
     response = input("\nХотите сгенерировать ещё один пароль? (y/n)\n>: ")
     if response.lower() in POSITIVE_ANSWERS:
         main()
